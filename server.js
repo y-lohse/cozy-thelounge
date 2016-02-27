7db633d5579ea620ea2210bbae70e5274462c7f9
Yannick Lohse <contact@yannick-lohse.fr>
2016-02-28 00:14:46 +0100

    move credentials into variable

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var loungeDir = 'node_modules/thelounge/',
 
 var lounge = new TheLounge(loungeDir, configDir);
 
+var userName = 'test',
+	password = '';
+
 //make sure there is a full config
 function checkConfig(){
 	fs.stat(configFile, function(err, stats){
@@ -29,9 +32,9 @@ function checkConfig(){
 	});
 }
 
+//cheks if we have created a user, and if not create one
 function checkUser(){
-	var userName = 'john',
-		userFile = configDir + '/users/' + userName + '.json';
+	var userFile = configDir + '/users/' + userName + '.json';
 	
 	fs.stat(userFile, function(err, stats){
 		if (err){
@@ -39,7 +42,7 @@ function checkUser(){
 			
 			userHandler.stdout.on('data', function(message){
 				//contrary to what the doc says, the password has to be inputed manually
-				if (message.match(/^Password/)) userHandler.stdin.write("test\n");
+				if (message.match(/^Password/)) userHandler.stdin.write(password + "\n");
 				//and then for whatever reason, the process doesn't terminate after the user is created
 				else if (message.match(new RegExp("^User '" + userName + "' created"))) userHandler.kill();
 			});
